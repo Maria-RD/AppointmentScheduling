c98b126ee3f6bb595b74a5758378d46133128ffa
Maria Rozo <50882747+Maria-RD@users.noreply.github.com>
2023-06-21 20:52:39 -0500

    feat: define constraints in DbContext for the table Speciality (#28)

--- Models/TurnsContext.cs
+++ Models/TurnsContext.cs
@@ -11,5 +11,20 @@ namespace Turns.Models
         }
 
         public DbSet<Speciality> Speciality { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Speciality>(entity =>
+            {
+                entity.ToTable("Speciality");
+
+                entity.HasKey(e => e.SpecialityId);
+
+                entity.Property(e => e.Description)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            });
+        }
     }
 }
