fbfa8f27e836e9d587c99e4d8d53dad3569cba29
Maria Rozo <rozo.maria.1001@gmail.com>
2023-06-24 19:24:38 -0500

    feat: add Physician model - pre-scaffolding

---
+++ Models/Physician.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TurnsBackFront.Models
+{
+    public class Physician
+    {
+        [Key]
+        public int PhysicianId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime OpeningHoursFrom { get; set; }
+        public DateTime OpeningHoursTo { get; set; }
+    }
+}
