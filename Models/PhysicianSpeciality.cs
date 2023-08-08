04dfe192adfced5a34658a49f9744847c70753c0
Maria Rozo <50882747+Maria-RD@users.noreply.github.com>
2023-08-07 19:47:18 -0500

    feat: add PhysicianSpeciality ER / add SQL project (#37)
    Co-authored-by: Maria Paula Rozo Daza <maria.rozo@gse.com.co>

---
+++ Models/PhysicianSpeciality.cs
@@ -0,0 +1,10 @@
+﻿namespace TurnsBackFront.Models
+{
+    public class PhysicianSpeciality
+    {
+        public int PhysicianId { get; set; }
+        public int SpecialityId { get; set; }
+        public Physician Physician { get; set; }
+        public Speciality Speciality { get; set; }
+    }
+}
