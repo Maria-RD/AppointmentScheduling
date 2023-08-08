21777bba2966231b6048a4c9374165ec9c72f5ee
Maria Rozo <50882747+Maria-RD@users.noreply.github.com>
2023-08-08 13:30:31 -0500

    Pbi 0037 containerize repo with docker in src folder (#43)
    * Update README.md
    
    * feat: containerize repo with Docker

---
+++ TurnsDB/TurnsDB/dbo/Tables/PhysicianSpeciality.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[PhysicianSpeciality] (
+    [PhysicianId]  INT NOT NULL,
+    [SpecialityId] INT NOT NULL,
+    CONSTRAINT [PK_PhysicianSpeciality] PRIMARY KEY CLUSTERED ([PhysicianId] ASC, [SpecialityId] ASC),
+    CONSTRAINT [FK_PhysicianSpeciality_Physician_PhysicianId] FOREIGN KEY ([PhysicianId]) REFERENCES [dbo].[Physician] ([PhysicianId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_PhysicianSpeciality_Speciality_SpecialityId] FOREIGN KEY ([SpecialityId]) REFERENCES [dbo].[Speciality] ([SpecialityId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_PhysicianSpeciality_SpecialityId]
+    ON [dbo].[PhysicianSpeciality]([SpecialityId] ASC);
+
