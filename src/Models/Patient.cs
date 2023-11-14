93b5786009c27bfe80a85d1b58241f7af4dfc165
Maria Rozo <rozo.maria.1001@gmail.com>
2023-11-13 22:36:12 -0500

    feat: add data annotations to physician, patient and speciality models

--- src/Models/Patient.cs
+++ src/Models/Patient.cs
@@ -6,10 +6,16 @@ namespace TurnsBackFront.Models
     {
         [Key]
         public int PatientId { get; set; }
+        [Required (ErrorMessage="You must enter a name")]
         public string Name { get; set; }
+        [Required (ErrorMessage="You must enter a surname")]
         public string Surname { get; set; }
+        [Required (ErrorMessage="You must enter an address")]
         public string Address { get; set; }
+        [Required (ErrorMessage="You must enter a phone")]
         public string Phone { get; set; }
+        [Required (ErrorMessage="You must enter an email")]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
--- src/Models/Physician.cs
+++ src/Models/Physician.cs
@@ -8,13 +8,26 @@ namespace TurnsBackFront.Models
     {
         [Key]
         public int PhysicianId { get; set; }
+        [Required (ErrorMessage="You must enter a name")]
         public string Name { get; set; }
+        [Required (ErrorMessage="You must enter a surname")]
         public string Surname { get; set; }
+        [Required (ErrorMessage="You must enter an address ")]
         public string Address { get; set; }
+        [Required (ErrorMessage="You must enter a phone")]
         public string Phone { get; set; }
+        [Required (ErrorMessage="You must enter an email")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Display (Name = "Opening hours")]
+        [DataType (DataType.Time)]
+        [DisplayFormat (DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime OpeningHoursFrom { get; set; }
+        [Display (Name = "Closing hours")]
+        [DataType (DataType.Time)]
+        [DisplayFormat (DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime OpeningHoursTo { get; set; }
+        [Required (ErrorMessage="You must enter the speciality")]
         public List<PhysicianSpeciality> PhysicianSpeciality { get; set; }
     }
 }
